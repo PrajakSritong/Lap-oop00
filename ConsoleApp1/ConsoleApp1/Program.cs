29eaebc07d76ca61920eb136718df548489a7a6a
NitchakarnT <96216803+NitchakarnT@users.noreply.github.com>
2022-11-28 15:52:49 +0700

    add id name email

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ประจักษ์ ศรีทอง");
+Console.WriteLine("653450294-0");
+Console.WriteLine("Prajak.sr@kkumail.com");
